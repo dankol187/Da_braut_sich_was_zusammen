bc8b2735c9b26997d62b8f547a8c99b3fdddbaa4
JezLynn <JezLynn@users.noreply.github.com>
2025-05-27 09:51:09 +0200

    Update update.php
    We need to use system instead

--- update.php
+++ update.php
@@ -1,9 +1,14 @@
 <?php
-// outputs the username that owns the running php/httpd process
-// (on a system with the "whoami" executable in the path)
-$output=null;
-$retval=null;
-exec('whoami', $output, $retval);
-echo "Returned with status $retval and output:\n";
-print_r($output);
+echo '<pre>';
+
+// Outputs all the result of shellcommand "ls", and returns
+// the last output line into $last_line. Stores the return value
+// of the shell command in $retval.
+$last_line = system('ls', $retval);
+
+// Printing additional info
+echo '
+</pre>
+<hr />Last line of the output: ' . $last_line . '
+<hr />Return value: ' . $retval;
 ?>
