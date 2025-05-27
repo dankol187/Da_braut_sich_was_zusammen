811ec3bf64e714e79185b756f586b9709af15265
aareif <aareif@schuldorf.de>
2025-05-27 09:18:03 +0200

    Update test.php

--- test.php
+++ test.php
@@ -3,16 +3,17 @@ require_once 'Database.php';
 $db = new Database();
 $db->connect();
 
-$sql = "SELECT * FROM Item"; // Passe den Tabellennamen an
-$result = $db->query($sql);
 
-if ($result) {
-    foreach ($result as $row) {
-        print_r($row);
-        echo "<br>";
+$sql = "SELECT id, name FROM Item";  // Passe den Tabellennamen an
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // Ausgabe der Daten jeder Zeile
+    while($row = $result->fetch_assoc()) {
+        echo "ID: " . $row["id"]. " - Name: " . $row["name"]. "<br>";
     }
 } else {
-    echo "Fehler bei der Abfrage!";
+    echo "0 Ergebnisse";
 }
 
 $db->disconnect();
