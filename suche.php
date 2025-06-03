250ad6d45b776b52166d4fa0d8037456cf63f310
aareif <aareif@schuldorf.de>
2025-06-03 09:08:39 +0200

    Update suche.php

--- suche.php
+++ suche.php
@@ -3,30 +3,12 @@ require_once 'Database.php';
 $db = new Database();
 $conn = $db->connect();
 
-// Fehlerüberprüfung
-if ($conn->connect_error) {
-    die("Verbindung fehlgeschlagen: " . $conn->connect_error);
-}
-
 if (isset($_GET['suchbegriff'])) {
     $suchbegriff = $conn->real_escape_string($_GET['suchbegriff']);
     
     $sql = "SELECT id, name FROM Item WHERE name LIKE '%$suchbegriff%'";
     $result = $conn->query($sql);
-
-    if ($result->num_rows > 0) {
-        echo "<ul>";
-        while($row = $result->fetch_assoc()) {
-            echo "<li>" . htmlspecialchars($row["name"]) . "</li>";
-        }
-        echo "</ul>";
-    } else {
-        echo "Keine Ergebnisse gefunden.";
-    }
-}
-
-    
-
+} 
 $db->disconnect();
 ?>
 
@@ -43,9 +25,9 @@ $db->disconnect();
     </form>
 <h2>Suchergebnisse:</h2>
     <?php if (isset($_GET['suchbegriff'])): ?>
-            <?php if (count($result) > 0): ?>
+            <?php if ($result->num_rows > 0): ?>
             <ul>
-                <?php foreach ($result as $row): ?>
+                <?php while ($row = $result->fetch_assoc()): ?>
                     <li>ID: <?php echo $row['id']; ?> - Name: <?php echo htmlspecialchars($row['name']); ?></li>
     <?php endforeach; ?>        
                 </ul>
