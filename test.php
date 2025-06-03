bda15dbb75f67d1342eb03a3d9d24612ad85e139
aareif <aareif@schuldorf.de>
2025-06-03 08:59:13 +0200

    Update test.php

--- test.php
+++ test.php
@@ -4,7 +4,7 @@ $db = new Database();
 $conn = $db->connect();
 
 
-$sql = "SELECT Brennstoff.ID, name FROM Brennstoff";  // Passe den Tabellennamen an
+$sql = "SELECT Brennstoff.ID FROM Brennstoff";  // Passe den Tabellennamen an
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
