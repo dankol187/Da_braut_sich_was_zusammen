86d83015c6697a4eb471eb23b47d0a732f603cc0
aareif <aareif@schuldorf.de>
2025-06-05 12:08:00 +0200

    Update nutzer_registrierung.php

--- nutzer_registrierung.php
+++ nutzer_registrierung.php
@@ -17,7 +17,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         echo "Benutzername ist bereits vergeben.";
     } else {
         // Benutzer in die Datenbank einfügen
-        $stmt = $conn->prepare("INSERT INTO Nutzer (Benutzername, EMail, Passwort) VALUES (:username, :email, :password)");
+        $stmt = $conn->prepare("INSERT INTO Nutzer (Benutzername, EMail, Passwort) VALUES ('username', 'email', 'password')");
         $stmt->execute(['Benutzername' => $username, 'EMail' => $email, 'Passwort' => $password]);
         echo "Registrierung erfolgreich!";
         header("Location: index.php"); // Weiterleitung zur Startseite
