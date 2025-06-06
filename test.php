568c6b3cad12f6de8d78801682c21c56ad2950a3
aareif <aareif@schuldorf.de>
2025-06-06 10:21:04 +0200

    Update test.php

--- test.php
+++ test.php
@@ -4,13 +4,13 @@ $db = new Database();
 $conn = $db->connect();
 
 
-$sql = "SELECT BrennstoffID FROM Brennstoff";  // Passe den Tabellennamen an
+$sql = "SELECT * FROM Trank";  // Passe den Tabellennamen an
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     // Ausgabe der Daten jeder Zeile
     while($row = $result->fetch_assoc()) {
-        echo "BrennstoffID: " . $row["BrennstoffID"]. " - Name: " . $row["name"]. "<br>";
+        echo "TrankID: " . $row["TrankID"]. " - Name: " . $row["name"].  " - Art: " . $row["art"]. " - Stufe: " . $row["stufe"]. " - Beschreibung: " . $row["beschreibung"]." - Dauer: " . $row["dauer"]."<br>";
     }
 } else {
     echo "0 Ergebnisse";
