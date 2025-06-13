9ce569f6e3a58a028e684e159b43e7a24a652562
dankol187 <dankol@schuldorf.de>
2025-06-13 10:06:59 +0200

    Update logout_erfolgreich.php

--- logout_erfolgreich.php
+++ logout_erfolgreich.php
@@ -7,7 +7,7 @@ session_destroy(); // Alle Sitzungsvariablen zerstören
 <html lang="de">
 <head>
 <meta charset="UTF-8">
-<title>Logout erfolgreich</title>
+<title>Abmeldung erfolgreich</title>
 <style>
 body {font-family: Arial, sans-serif;}
 .container {margin: 50px auto; max-width: 400px; padding: 20px; border: 1px solid #dadada; border-radius: 8px; background: #f9f9f9;}
@@ -16,8 +16,8 @@ body {font-family: Arial, sans-serif;}
 </head>
 <body>
 <div class="container">
-<h2>Logout erfolgreich!</h2>
-<p>Du wurdest ausgeloggt.</p>
+<h2>Abmeldung erfolgreich!</h2>
+<p>Du wurdest abgemeldet.</p>
 <form action="nutzer_login.php" method="get">
 <button class="button" type="submit">Zurück zur Anmeldung</button>
 </form>
